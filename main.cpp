630ff5b0234865f03f6b56c6a7ca9b78546c0691
gabijatuma <60934852+gabijatuma@users.noreply.github.com>
2020-02-11 19:09:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,35 +5,39 @@ int main() {
   string greet = "Sveikas,";
   cout << "Iveskite savo varda: "<<endl;
   cin >> name;
+  int length = name.size();
+  if(name[length-1]=='a')
+  greet = "Sveika, ";
+
   int n = greet.length();
   n+=name.length();
   n+=5;
-
-  string one, two, three, four, five;
-
+  int k, z;
+  cout << "Kokio plocio remelio norite?";
+  cin>>k;
+  k--;
+  z = k-k/2;
   for(int i = 0; i < n; i++)
-    one+='*';
-  two+='*';
-  for(int i = 0; i < n-2; i++)
-    two+=' ';
-  two+='*';
-  three+="* ";
-  three+=greet;
-  three+=" ";
-  three+=name;
-  three+=" *";
-    four+="*";
-  for(int i = 0; i < n-2; i++)
-    four+=" ";
-  four+="*";
+    cout<<"*";
+  cout<<endl;
+
+  for(int i = 0; i <k/2; i++)
+  {
+      cout<<"*";
+      for(int i = 0; i < n-2; i++)
+        cout<<" ";
+      cout<<"*"<<endl;
+  }
+
+  cout<<"* "<<greet<<" "<<name<<" *"<<endl;
+
+for(int i = 0; i <z; i++)
+  {
+      cout<<"*";
+      for(int i = 0; i < n-2; i++)
+        cout<<" ";
+      cout<<"*"<<endl;
+  }
   for(int i = 0; i < n; i++)
-  five+="*";
-
-
-
-  cout<<one<<endl;
-  cout<<two<<endl;
-  cout<<three<<endl;
-  cout<<four<<endl;
-  cout<<five<<endl;
+  cout<<"*";
 }
