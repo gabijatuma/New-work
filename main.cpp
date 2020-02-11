0854ad8d362eef442c8b32c41ca7cc2ac9ed3d7a
gabijatuma <60934852+gabijatuma@users.noreply.github.com>
2020-02-11 18:19:37 +0200

    First task

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main() {
+  string name;
+  string greet = "Sveikas,";
+  cout << "Iveskite savo varda: "<<endl;
+  cin >> name;
+  int n = greet.length();
+  n+=name.length();
+  n+=5;
+
+  string one, two, three, four, five;
+
+  for(int i = 0; i < n; i++)
+    one+='*';
+  two+='*';
+  for(int i = 0; i < n-2; i++)
+    two+=' ';
+  two+='*';
+  three+="* ";
+  three+=greet;
+  three+=" ";
+  three+=name;
+  three+=" *";
+    four+="*";
+  for(int i = 0; i < n-2; i++)
+    four+=" ";
+  four+="*";
+  for(int i = 0; i < n; i++)
+  five+="*";
+
+
+
+  cout<<one<<endl;
+  cout<<two<<endl;
+  cout<<three<<endl;
+  cout<<four<<endl;
+  cout<<five<<endl;
+}
